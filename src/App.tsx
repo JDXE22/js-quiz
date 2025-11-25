47cae36f654ad6df9b1cc34e6a577812ee756f15
JuanDavid <davidesparzaj22@gmail.com>
2025-11-25 13:49:43 -0500

    refactor(src\App.tsx): refactor App layout with MUI components
    Replaces the basic HTML structure with Material UI's Container, Stack, and Typography for improved layout and styling. Adds the JavaScriptLogo component to the header section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
 import './App.css';
+import { Container, Stack, Typography } from '@mui/material';
+import { JavaScriptLogo } from './JavaScriptLogo';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Javascript Quizz</h1>
-    </>
+    <main>
+      <Container maxWidth='sm'>
+        <Stack
+          direction='row'
+          gap={2}
+          alignItems='center'
+          justifyContent='center'>
+          <JavaScriptLogo />
+          <Typography variant='h2' component='h1'>
+            Javascript Quizz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
   );
 }
 
